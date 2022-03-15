3a2cd6f674f1b35cbfcda1aa8633a60e9b9d0bed
GabrieleGarlaschelli <45970607+GabrieleGarlaschelli@users.noreply.github.com>
2022-03-15 12:48:22 +0100

    adding some tests

---
+++ TASKS.md
@@ -0,0 +1,16 @@
+# challenges
+Complete the challenges before and prove what you can do:
+
+## challenge 2
+
+Clone and make the project works.
+
+## challenge 2
+
+1. Create a component that contains a button called 'click me!', and save clicks number and display it below the button
+2. Make sure component is reachable from the homepage by a link called 'users clicks counter'
+
+## challenge 3
+
+There's a file in the project call src/components/ToBeFixed.vue. This file is coded very bad, by people who don't really know how to their job.
+The component should have a simple TODO list, you can add a task to the list, you can remove it, you can hide it. Fix the component in order to make it works.
---
+++ src/components/ToBeFixed.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <input type="text" v-model="addingTask"/>
+    <button @click="addTask(addingTask.name)"></button>
+    <ul>
+        <li v-for="task in taskCheSonoVisibili">
+            {{task.name}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ToBeFixed',
+  data: function() {
+    return {
+      tasks: [],
+      addingTask: undefined
+    }
+  },
+  methods: {
+    addTask(taskName) {
+        tasks.push({
+            name: taskName
+        })
+    },
+    removeTask(taskName) {
+        const index = this.tasks.indexOf({name: taskName})
+        this.tasks = this.tasks.slice(index, 0)
+    }
+  },
+  computed: {
+      allTasks: function() {
+        return this.tasks.filter(t => !!t.name && !!t.visible)   
+      },
+      taskCheSonoVisibili: function() {
+        return this.tasks.filter(t => !!t.name)
+      },
+      taskInvisibili: function() {
+        return this.tasks.filter(t => !t.visibile)
+      },
+  }
+};
+</script>
